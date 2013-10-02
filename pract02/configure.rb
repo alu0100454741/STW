a24d39ffead915c747e2da09909e21859bafbfcd
Javier Mena <alu0100454741@ull.edu.es>
2013-10-02 13:55:49 +0100

    Arreglando Twitter

---
+++ pract02/configure.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key       = '9Y2fwnzu3wp9c8UpON2ng'
+  config.consumer_secret    = 'pfkyeYMYl7ESyTLDkYNMK7oaHErTODlBeUVuIiB1M'
+  config.oauth_token        = "210796768-qas2zN0seX39Sa16XMFTSd0FeCBT3dAPjqLQ8Tw7"
+  config.oauth_token_secret = "LNMtgBlnebXHoo8TKlGIfvdQx1pAB8QO9k6srKY"
+end
