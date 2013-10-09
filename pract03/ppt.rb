61468506e4b7f416c94cf444984bbed671fdcfea
Javier Mena <alu0100454741@ull.edu.es>
2013-10-09 23:12:33 +0100

    cambiando haml

--- pract03/ppt.rb
+++ pract03/ppt.rb
@@ -4,57 +4,40 @@ require 'haml'
  
 module RockPaperScissors
   class App 
-    
+
+#Iicializacion del entorno
     def initialize(app = nil)
       @app = app
       @content_type = :html
-      @defeat = {'rock' => 'scissors', 'paper' => 'rock', 'scissors' => 'paper'}
+      @defeat = {'roca' => 'tijeras', 'papel' => 'roca', 'tijeras' => 'papel'}
       @throws = @defeat.keys
-      @choose = @throws.map { |x| 
-        %Q{ <li><a href="/?choice=#{x}">#{x}</a></li> }
-      }.join("\n")
-      @choose = "<p>\n<ul>\n#{@choose}\n</ul>"
     end
 
+
+#Función que
     def call(env)
-      req = Rack::Request.new(env) 
-      
-      req.env.keys.sort.each { |x| puts "#{x} => #{req.env[x]}" }
-      
+      req = Rack::Request.new(env)  
+      req.env.keys.sort.each { |x| puts "#{x} => #{req.env[x]}" }  
       computer_throw = @throws.sample
       player_throw = req.GET["choice"]
       answer = if !@throws.include?(player_throw)
-          "Choose one of the following:"
+        "Seleccione para Empezar"
         elsif player_throw == computer_throw
-          "You tied with the computer"
+          "Ha empatado con el Ordenador"
         elsif computer_throw == @defeat[player_throw]
-          "Nicely done; #{player_throw} beats #{computer_throw}"
+          "Bien Hecho => #{player_throw} gana a #{computer_throw}"
         else
-          "Ouch; #{computer_throw} beats #{player_throw}. Better luck next time!"
+          "Mala Suerte => #{computer_throw} gana a #{player_throw}"
         end
 
       res = Rack::Response.new
-      engine = Haml::Engine.new File.open("views/index.haml").read
-#       res = Rack::Response.new 
-      res.write engine.render({}, 
-        :answer => answer, 
-        :choose => @choose,
-        :throws => @throws,
-	:player_throw => player_throw,
-	:computer_throw => computer_throw)
-      res.finish
-          
-#       res.write <<-"EOS"
-#       <html>
-#         <title>Practica 3_STW</title>
-#         <body>
-#           <h1>
-#               #{anwser}
-#               #{@choose}
-#           </h1>
-#         </body>
-#       </html>
-#       EOS
+        engine = Haml::Engine.new File.open("views/index.haml").read
+        res.write engine.render({}, 
+          :answer => answer, 
+          :throws => @throws,
+          :player_throw => player_throw, 
+          :computer_throw => computer_throw)
+        res.finish
       res.finish
     end # call
   end   # App
@@ -63,11 +46,5 @@ end     # RockPaperScissors
 if $0 == __FILE__
   require 'rack'
   require 'rack/showexceptions'
-  Rack::Server.start(
-    :app => Rack::ShowExceptions.new(
-              Rack::Lint.new(
-                RockPaperScissors::App.new)), 
-    :Port => 9292,
-    :server => 'thin'
-  )
+  Rack::Server.start(:app => Rack::ShowExceptions.new(Rack::Lint.new(RockPaperScissors::App.new)),:Port => 9292,:server => 'thin')
 end
