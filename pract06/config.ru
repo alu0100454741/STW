d3d120cfae4f90f1c8182741dfd78887b2c96d0b
Javier Mena <alu0100454741@ull.edu.es>
2013-10-29 16:51:02 +0000

    finalizando cambios en las sesiones

--- pract06/config.ru
+++ pract06/config.ru
@@ -1,15 +1,13 @@
 require './lib/rsack/ppt.rb'
 
-builder = Rack::Builder.new do
-  use Rack::Static, :urls => ['/public']
   use Rack::ShowExceptions
   use Rack::Lint
   use Rack::Session::Cookie, 
-      :key => 'rack.session', 
-      :domain => 'localhost:8080',
-      :secret => 'some_secret'
+	  :key => 'rack.session', 
+	  :domain => 'example.com',
+	  :secret => 'some_secret'
+  use Rack::Static, :urls => ['/public']
 
   run RockPaperScissors::App.new
-end
 
-Rack::Handler::Thin.run builder
+
--- pract06/lib/rsack/ppt.rb
+++ pract06/lib/rsack/ppt.rb
@@ -5,6 +5,19 @@ require 'haml'
 module RockPaperScissors
   class App
 
+    def set_env(env)
+      @env = env
+      @session = env['rack.session']
+    end
+
+    def some_key 
+      return @session['some_key'].to_i if @session['some_key']
+      @session['some_key'] = 0
+    end
+
+    def some_key=(value)
+      @session['some_key'] = value
+    end
 
 #Iicializacion del entorno
     def initialize(app = nil)
@@ -15,26 +28,12 @@ module RockPaperScissors
       @estadistica = {'empatar' => 0, 'ganar' => 0, 'perder' => 0}
     end
 
-    # def set_env(env)
-    #   @env = env
-    #   @session = env['rack.session']
-    # end
-
-    # def some_key 
-    #   return @session['some_key'].to_i if @session['some_key']
-    #   @session['some_key'] = 0
-    # end
-
-    # def some_key=(value)
-    #   @session['some_key'] = value
-    # end
-
 
 #Función que
     def call(env)
-      # set_env(env)
+      set_env(env)
       req = Rack::Request.new(env)  
-      # req.env.keys.sort.each { |x| puts "#{x} => #{req.env[x]}" }  
+      req.env.keys.sort.each { |x| puts "#{x} => #{req.env[x]}" }  
 
       computer_throw = @throws.sample
       player_throw = req.GET["choice"]
@@ -54,16 +53,14 @@ module RockPaperScissors
       engine = Haml::Engine.new File.open("views/index3.haml").read
       res = Rack::Response.new
 
-      res.set_cookie("menaser89", {:value => @estadistica, :path => "/", :expires => Time.now+24*60*60})
-      #self.some_key = self.some_key + 1 if req.path == '/'
-      #res.write("some_key = #{@session['some_key']}\n")
-
+      self.some_key = self.some_key + 1 if req.path == '/'
       res.write engine.render({}, 
         :estadistica => @estadistica,
         :answer => answer, 
         :throws => @throws,
         :player_throw => player_throw, 
-        :computer_throw => computer_throw)
+        :computer_throw => computer_throw,
+        :some_key => some_key)
       res.finish
     end # call
   end   # App
